ea8f429a20f9d8d41650c250b31b0c65a4856bdf
fixingSegmentation <mudit.rajesh@gmail.com>
2020-10-13 23:05:18 +0530

    Modified code to remove errors.(vecdiff.c)

--- vecdiff.c
+++ vecdiff.c
@@ -6,7 +6,3 @@ void diff(vector vec_a, vector vec_b, vector vec_c) {
         vec_c[i] = vec_a[i] - vec_b[i];
     }
 }
-
-void eleProd(vector vec_a, vector vec_b, vector vec_c) {}
-float dotprod(vector vec_a, vector vec_b) {return 0;}
-float angle(vector vec_a, vector vec_b) {return 0;}
