0a8d0b91f6e6657dbf6ea1db6572ac7441003ac3
yash <yash@Apples-MacBook-Pro.local>
2020-10-16 19:05:05 +0530

    added code for vector angle (vecangle.c)

--- vecangle.c
+++ vecangle.c
@@ -5,6 +5,6 @@
 float angle(vector vec_a, vector vec_b)
 {
     float Cos0 = dotprod(vec_a,vec_b)/(norm(vec_a)*norm(vec_b));
-    float ang_def = acos(Cos0);
+    float ang_deg = acos(Cos0);
     return ang_def;
 }
