c25619351383208e058283d48420ed3cd2015af9
yash <yash@Apples-MacBook-Pro.local>
2020-10-16 18:51:25 +0530

    added code for vector angle (vecangle.c)

--- vecangle.c
+++ vecangle.c
@@ -4,7 +4,7 @@
 // function to calculate angle between two vectos in radians.
 float angle(vector vec_a, vector vec_b)
 {
-    float Cos0 = max(min(dotprod(vec_a,vec_b)/(norm(vec_a)*norm(vec_b)),1),-1);
-    float ang_Rad = real(acos(Cos0));
-    return ang_Rad;
+    float Cos0 = dotprod(vec_a,vec_b)/(norm(vec_a)*norm(vec_b));
+    float ang_def = acos(Cos0);
+    return ang_def;
 }
