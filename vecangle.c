cb87171a45ddba97b5ee224e300cbf4db976501b
yash <yash@Apples-MacBook-Pro.local>
2020-10-16 19:14:10 +0530

    added code for vector angle (vecangle.c)

--- vecangle.c
+++ vecangle.c
@@ -1,7 +1,7 @@
 #include "veclib.h"
 #include <math.h>
 
-// function to calculate angle between two vectos in radians.
+// function to calculate angle between two vectos in degree.
 float angle(vector vec_a, vector vec_b)
 {
     float Cos0 = dotprod(vec_a,vec_b)/(norm(vec_a)*norm(vec_b));
