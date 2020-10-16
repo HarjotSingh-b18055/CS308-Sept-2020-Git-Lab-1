1fef8dab33ff95d8f75d27505b89f6e18babe80b
ShikhaSuman8601 <shikhadarshi@gmail.com>
2020-10-16 13:07:10 +0530

    Added function for dot product of two vectors in vecdot.c

---
+++ vecdot.c
@@ -0,0 +1,15 @@
+/* vecdot.c */
+/* Auther: github.com/ShikhaSuman8601 */
+
+// function to calculate the dot product of two vectors
+
+#include "veclib.h"
+
+float dotprod(vector vec_a, vector vec_b)
+{
+	float ans=0.0;
+	for (int i=0; i<DIM; i++){
+		ans += (vec_a[i]*vec_b[i]);
+	}
+	return ans;
+}
